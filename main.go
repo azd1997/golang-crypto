7402a940909016b74087eb761024b6d7fdf2f4f6
Eiger <33643657+azd1997@users.noreply.github.com>
2019-05-07 15:28:44 +0800

    DES
    Signed-off-by: azd1997 <374192922@qq.com>

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/azd1997/golang-crypto/my_crypto"
+)
+
+//main.go用来测试各个加密算法
+
+func main() {
+	my_crypto.DesTest()
+}
---
+++ my_crypto/my-des-cbc-test.go
@@ -0,0 +1,13 @@
+package my_crypto
+
+import "fmt"
+
+//测试DES加解密
+func DesTest() {
+	fmt.Println("===== des 加解密测试")
+	src := []byte("少壮不努力，老大徒伤悲！")
+	key := []byte("12345678")
+	strByteSlice := EncryptDES(src, key)
+	strByteSlice = DecryptDES(strByteSlice, key)
+	fmt.Println("解密之后的明文：", string(strByteSlice))
+}
